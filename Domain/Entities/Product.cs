85e62cb260de2444a4c19ee36627adf2dd71ac03
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-05-06 08:50:36 +0545

    Day 2

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } 
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
