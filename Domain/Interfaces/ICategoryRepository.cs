72717e3ecfb2013c2b7290ad9329555cf8bd2648
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-05-07 09:00:11 +0545

    Day 3

---
+++ Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync(int id);
+        Task<List<Category>> GetAllSync();
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+        Task<List<Product>> GetProductsByCategoryIdAsync(int categoryId);
+    }
+}
---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+     public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(int id);
+        Task<List<Product>> GetAllSync();
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+        Task<List<Product>> GetLowStockAsync(int threshold);
+
+
+
+    }
+}
